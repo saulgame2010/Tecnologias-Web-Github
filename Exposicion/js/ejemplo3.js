7d5f0c66c6a88f42d722bbe7df6ce9c425d11c78
Saul <saulgame2010@gmail.com>
2021-04-28 01:46:47 -0500

    jajajajaja
    puras mamadas

--- Exposicion/js/ejemplo3.js
+++ Exposicion/js/ejemplo3.js
@@ -2,46 +2,51 @@
 
 function draw() {
     var canvas = document.getElementById("ejemplo3");
-    var context = canvas.getContext("2d");
+    var ctx = canvas.getContext("2d");
+
+    ctx.font = "7pt Helvetica";
+    ctx.fillStyle = "blue";
+    ctx.fillText("Como dijo el vladimir", 190, 30);
+    ctx.fillStyle = "red";
+    ctx.fillText("un padre nuestro y a dormir", 170, 50);
     // Cara
-    context.beginPath();
-    context.lineWidth = 10;
-    context.strokeStyle = "#0597F2";
-    context.arc(100, 75, 69, 0, 2 * Math.PI, true);
-    context.stroke();
+    ctx.beginPath();
+    ctx.lineWidth = 10;
+    ctx.strokeStyle = "#0597F2";
+    ctx.arc(100, 75, 69, 0, 2 * Math.PI, true);
+    ctx.stroke();
 
     // Color de la cara
-    context.beginPath();
-    context.fillStyle = "#F24607";
-    context.arc(100, 75, 69, 0, 2 * Math.PI, true);
-    context.fill();
+    ctx.beginPath();
+    ctx.fillStyle = "#F24607";
+    ctx.arc(100, 75, 69, 0, 2 * Math.PI, true);
+    ctx.fill();
 
     // ojo derecho
-    context.strokeStyle = "#49D907";
-    context.lineWidth = 10;
-    context.beginPath();
-    context.moveTo(80, 30);
-    context.bezierCurveTo(80, 30, 80, 30, 80, 70);
-    context.stroke();
+    ctx.strokeStyle = "#49D907";
+    ctx.lineWidth = 10;
+    ctx.beginPath();
+    ctx.moveTo(80, 30);
+    ctx.bezierCurveTo(80, 30, 80, 30, 80, 70);
+    ctx.stroke();
 
     // ojo izquierdo
-    context.strokeStyle = "#970FF2";
-    context.beginPath();
-    context.moveTo(115, 30);
-    context.bezierCurveTo(115, 30, 115, 30, 115, 70);
-    context.stroke();
+    ctx.strokeStyle = "#970FF2";
+    ctx.beginPath();
+    ctx.moveTo(115, 30);
+    ctx.bezierCurveTo(115, 30, 115, 30, 115, 70);
+    ctx.stroke();
 
     // labio superior
-    context.strokeStyle = "#EAF205";
-    context.beginPath();
-    context.moveTo(55, 70);
-    context.bezierCurveTo(55, 70, 110, 120, 145, 70);
-    context.stroke();
+    ctx.strokeStyle = "#EAF205";
+    ctx.beginPath();
+    ctx.moveTo(55, 70);
+    ctx.bezierCurveTo(55, 70, 110, 120, 145, 70);
+    ctx.stroke();
 
     // labio inferior
-    context.beginPath();
-    context.moveTo(55, 71);
-    context.bezierCurveTo(60, 75, 90, 200, 145, 70);
-    context.stroke();
+    ctx.beginPath();
+    ctx.moveTo(55, 71);
+    ctx.bezierCurveTo(60, 75, 90, 200, 145, 70);
+    ctx.stroke();
 }
-
