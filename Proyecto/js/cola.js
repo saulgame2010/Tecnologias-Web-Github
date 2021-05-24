f1f888568125c2ca9de2044196289da97b2d2f3e
Saul <saulgame2010@gmail.com>
2021-05-24 04:00:43 -0500

    La cola de la profa
    Ya está quedando un poco mejor la cola ajsjasjas vale berga puto proyecto chingatumadre y arriba las chibas

--- Proyecto/js/cola.js
+++ Proyecto/js/cola.js
@@ -8,71 +8,54 @@ window.onload = function() {
     dark.addEventListener('click', cambiarDark, false);    
     clear.addEventListener('click', cambiarClear, false);
     color.addEventListener('click', cambiarColor, false);    
-    const ponerCola = document.getElementById("ponerCola");
-    //ponerCola.addEventListener('click', comenzar, false);
+    const ponerCola = document.getElementById("ponerCola");    
     ponerCola.addEventListener('click', createElement, false);
     const quitarCola = document.getElementById("quitarCola");
     quitarCola.addEventListener('click', quitarElement, false);
 }
 
-/*function comenzar() {
-    var canvas = document.getElementById("canvas");
-    var lienzo = canvas.getContext("2d");    
-    animar = setInterval(function () { animarEsfera(lienzo); }, 50);    
-}
+    let texto;
+    let newDiv;
 
-function animarEsfera(ctx) {    
-    let texto = document.getElementById("elemento").value;
-    if(y <= 250){ 
-        ctx.clearRect(0, 0, 800, 500);
-        ctx.beginPath();
-        ctx.fillStyle = "#A6032F";
-        ctx.arc(x, y, 17, 0, Math.PI*2, false);
-        ctx.fill();  
-        ctx.fillStyle = "#FFFFFF";
-        ctx.font = "20px Verdana"
-        ctx.fillText(texto, x-4, y+4);             
-        y = y + 5;          
-    } else {        
-        clearInterval(animar);
-        dibujarEsfera(ctx, x, y, texto)
-    }    
-}
-
-function dibujarEsfera(ctx, x, y, texto) {
-    ctx.clearRect(0,0,800,500);          
-    ctx.beginPath();
-    ctx.fillStyle = "#A6032F";
-    ctx.arc(x, y, 17, 0, Math.PI*2, false);
-    ctx.fill(); 
-    ctx.fillStyle = "#FFFFFF";
-    ctx.font = "20px Verdana"
-    ctx.fillText(texto, x-4, y+4)
-}*/
-
-function createElement() {
-    let span = [...document.getElementsByTagName("SPAN")];
-    console.log(span)
-    let texto = document.getElementById("elemento").value;
-    let newDiv = document.createElement("div");    
-    newDiv.setAttribute("id", "nodos");    
+function createElement() {    
+    texto = document.getElementById("elemento").value;
+    newDiv = document.createElement("div");
+    newDiv.setAttribute("id", "nodo");
+    newDiv.setAttribute("class", "nodos");   
     let content = document.createTextNode(texto);        
-    newDiv.appendChild(content);        
-    let divActual = document.getElementById("animacion");
+    newDiv.appendChild(content);
+    //Si quieres animarlo de la otra forma, deberás borrar esta línea
     newDiv.style.animation = "slidein 2s";
-    for(let i; i < span.length; i++) {
-        span[i].style.animation = "resaltar 2s";
-    }
-    divActual.appendChild(newDiv);     
+    let divActual = document.getElementById("animacion");            
+    //Esto es parte de la otra forma de animarlo
+    //newDiv.style.marginBottom = "100%";
+    divActual.appendChild(newDiv);
+    document.getElementById("elemento").value = "";
+    //Esta es otra forma de animarlo >:(
+    //let cont = 0;
+    /*var animar = () => {               
+        if(cont < 1666){
+            var newMgBottom = (100 - ((cont)/1000) * 100);
+            newDiv.style.marginBottom = newMgBottom + "%";
+            cont+=10;
+            if(newMgBottom > 0) {
+                window.requestAnimationFrame(animar);
+            }   
+        }              
+    };
+    animar();*/
 }
 
-function quitarElement() {
-    let elemento = document.getElementById("nodos");           
+function quitarElement() {         
     if(!elemento) {
         alert("La cola está vacía");
-    } else {        
-        let padre = elemento.parentNode;        
-        padre.removeChild(elemento);
+    } else {
+        document.querySelector(".nodos").classList.add("ocultar");
+        setTimeout(() => {
+            let elemento = document.getElementById("nodo");
+            let padre = elemento.parentNode;        
+            padre.removeChild(elemento);
+        }, 1000);                
     }
 }
 
